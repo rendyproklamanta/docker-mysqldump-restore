8a2ab878bffa0556c3d465b1741b07c6f343647c
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-04-29 13:35:31 +0700

    update

--- import.sh
+++ import.sh
@@ -7,12 +7,19 @@ source env.sh
 docker cp $DB_NAME.sql.gz $(docker ps -q -f name=$DB_HOST):/
 
 echo 
-echo "Import $DB_NAME has started"
+echo "===[ Installing PV ]==="
 echo
 
 # exec to container
-docker exec -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME $(docker ps -q -f name=$DB_HOST) sh -c 'gunzip -c /$DB_NAME.sql.gz | mysql -P $DB_PORT -u root -p $DB_PASS $DB_NAME'
+docker exec $(docker ps -q -f name=$DB_HOST) sh -c 'apt-get update && apt-get install -y pv && rm -rf /var/lib/apt/lists/*'
 
 echo 
-echo "Import $DB_NAME finished"
+echo "===[ Import $DB_NAME has started ]==="
+echo
+
+# exec to container
+docker exec -it -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME $(docker ps -q -f name=$DB_HOST) sh -c 'pv $DB_NAME.sql.gz | gunzip | mysql -u root -p $DB_PASS $DB_NAME'
+
+echo 
+echo "===[ Import $DB_NAME finished ]==="
 echo
--- mysqldump.sh
+++ mysqldump.sh
@@ -4,15 +4,22 @@
 source env.sh
 
 echo 
-echo "Dumping $DB_NAME has started"
+echo "===[ Installing PV ]==="
 echo
 
 # exec to container
-docker exec -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME $(docker ps -q -f name=$DB_HOST) sh -c 'mysqldump -P $DB_PORT -u root -p $DB_PASS $DB_NAME -R -E --triggers --routines --single-transaction --quick | gzip -9 > $DB_NAME.sql.gz'
+docker exec $(docker ps -q -f name=$DB_HOST) sh -c 'apt-get update && apt-get install -y pv && rm -rf /var/lib/apt/lists/*'
+
+echo 
+echo "===[ Dumping $DB_NAME has started ]==="
+echo
+
+# exec to container
+docker exec -it -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME $(docker ps -q -f name=$DB_HOST) sh -c 'mysqldump -P $DB_PORT -u root -p $DB_PASS $DB_NAME -R -E --triggers --routines --single-transaction --quick | gzip -9 | pv > $DB_NAME.sql.gz'
 
 # copy file from container to local machine
 docker cp $(docker ps -q -f name=$DB_HOST):/$DB_NAME.sql.gz .
 
 echo 
-echo "Dumping $DB_NAME finished"
+echo "===[ Dumping $DB_NAME finished ]==="
 echo
