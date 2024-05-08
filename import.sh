683e9861941b6aec9fe353f70e1f7862be86b428
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-05-08 11:43:42 +0700

    update

--- import.sh
+++ import.sh
@@ -18,7 +18,7 @@ echo "===[ Import $DB_NAME has started ]==="
 echo
 
 # exec to container
-docker exec -it -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME $(docker ps -q -f name=$DB_HOST) sh -c 'pv $DB_NAME.sql.gz | gunzip | mysql -u root -p $DB_PASS $DB_NAME'
+docker exec -it -e DB_HOST=$DB_HOST -e DB_PORT=$DB_PORT -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME $(docker ps -q -f name=$DB_HOST) sh -c 'pv $DB_NAME.sql.gz | gunzip | mysql -uroot -p$DB_PASS $DB_NAME'
 
 echo 
 echo "===[ Import $DB_NAME finished ]==="
